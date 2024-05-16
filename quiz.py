7ede5966e7033e2fef4aef0ee1330e84e7caa271
Otero <oteronickbrian@gmail.com>
2024-05-17 02:06:07 +0300

    handle user errors

--- quiz.py
+++ quiz.py
@@ -81,6 +81,7 @@ QUESTIONS = {
     ],
 }
 
+num_correct = 0
 for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
     print(f"\nQuestion {num}:")
     print(f"{question}?")
@@ -88,10 +89,15 @@ for num, (question, alternatives) in enumerate(QUESTIONS.items(), start=1):
     labeled_alternatives = dict(zip(ascii_lowercase, sorted(alternatives)))
     for label, alternative in labeled_alternatives.items():
         print(f"  {label}) {alternative}")
+        
+    while (answer_label := input("\nChoice? ")) not in labeled_alternatives:
+        print(f"Please answer one of {', '.join(labeled_alternatives)}")
 
-    answer_label = input("\nChoice? ")
-    answer = labeled_alternatives.get(answer_label)
+    answer = labeled_alternatives[answer_label]
     if answer == correct_answer:
+        num_correct += 1
         print("⭐ Correct! ⭐")
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
+
+print(f"\nYou got {num_correct} correct out of {num} questions")
